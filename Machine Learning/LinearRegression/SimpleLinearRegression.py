649ce4d01c8f36bb84ccae1f40361687eaadcf83
bfl3tcher <bfletch3r@gmail.com>
2019-05-21 20:44:37 -0700

    lin regression changes

--- Machine Learning/LinearRegression/SimpleLinearRegression.py
+++ Machine Learning/LinearRegression/SimpleLinearRegression.py
@@ -48,6 +48,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=.3, random_state=101)
 
 
+# # standard scaler
+# from sklearn.preprocessing import StandardScaler
+
+# sc_X = StandardScaler()    # import standard scaler
+# X_train = sc_X.fit_transform(X_train)    # scale X train
+# X_test = sc_X.transform(X_test)    # scale X test
+
+
 # hide runtime warnings - harmless bug
 import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
@@ -58,11 +66,11 @@ from sklearn.linear_model import LinearRegression    # linear regression model
 lm = LinearRegression()    # instantiate Linear Regressor
 lm.fit(X_train, y_train)    # fit training data to model
 
-# reporting
-print('Intercept', lm.intercept_)    # LM intercept
+# # reporting
+# print('Intercept', lm.intercept_)    # LM intercept
 
-cdf = pd.DataFrame(lm.coef_, X.columns, columns=['Coeff'])    # coefficients
-print('\n\t\tCoefficients\n\n', cdf)    # print dataframe of coefficients
+# cdf = pd.DataFrame(lm.coef_, X.columns, columns=['Coeff'])    # coefficients
+# print('\n\t\tCoefficients\n\n', cdf)    # print dataframe of coefficients
 
 
 
@@ -79,6 +87,32 @@ import seaborn as sns
 
 # graphing results
 predictions = lm.predict(X_test)    # save predictions
+
+
+'''
+Model Evaluation - Metrics
+-MAE - Mean Absolute Error
+-MSE - Mean Squared Error
+-RMSE - Root Mean Square Error
+'''
+# reporting metrics
+from sklearn import metrics
+
+
+# The coefficients
+print('\n-----------------------------------------------------')
+print('\tMetrics')
+print('Intercept:', lm.intercept_)    # LM intercept
+print('Variance score (R2): %.2f' % r2_score(y_test, predictions))
+print('\n\tFeatures')
+coef = pd.DataFrame(data=list(lm.coef_)[0], index=X.columns, columns=['Coef'])
+print(coef.head())
+print('\n-----------------------------------------------------')
+print('MAE: ', metrics.mean_absolute_error(y_test, predictions))    # Mean Absolute Error
+print('MSE: ', metrics.mean_squared_error(y_test, predictions))    # Mean Squared Error
+print('RMSE: ', np.sqrt(metrics.mean_squared_error(y_test, predictions)))    # Root Mean Square Error
+print('\n-----------------------------------------------------')
+
 
 # graphing predicted vs actual
 plt.scatter(x=predictions,y=y_test)
@@ -97,19 +131,3 @@ plt.show()
 # shows correlation with all the columns
 sns.heatmap(df.corr(), cmap='coolwarm', annot=True)
 plt.show()
-
-
-
-'''
-Model Evaluation - Metrics
--MAE - Mean Absolute Error
--MSE - Mean Squared Error
--RMSE - Root Mean Square Error
-'''
-# reporting metrics
-from sklearn import metrics
-
-print('Metrics')
-print('MAE: ', metrics.mean_absolute_error(y_test, predictions))    # Mean Absolute Error
-print('MSE: ', metrics.mean_squared_error(y_test, predictions))    # Mean Squared Error
-print('RMSE: ', np.sqrt(metrics.mean_squared_error(y_test, predictions)))    # Root Mean Square Error
