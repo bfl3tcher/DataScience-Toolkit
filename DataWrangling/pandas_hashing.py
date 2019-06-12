72faf86f242b487e1865cd408406a403eff039be
bfl3tcher <bfletch3r@gmail.com>
2019-06-12 16:44:16 -0700

    added hashing modules

---
+++ DataWrangling/pandas_hashing.py
@@ -0,0 +1,10 @@
+'''
+- Hash tool for Pandas
+- https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.util.hash_pandas_object.html
+-
+- 
+'''
+
+
+from pandas.util import hash_pandas_object
+h = hash_pandas_object(df)
---
+++ DataWrangling/sha1_hashing.py
@@ -0,0 +1,43 @@
+'''
+Function for sha1 hashing
+--- Only does sha1 hashing
+  - replace sha1 below with diff has algorithm
+    - hashlib.sha1()
+
+-- Inputs for function -
+--- currently requires input for all of these
+    - dataframe = source dataframe
+    - source_column = name of column being hashed
+    - salt = code prepended to column that is hashed
+    - new_column = name of column being created
+'''
+# import libraries
+import hashlib
+import pandas as pd
+
+
+# load data
+fn = ''
+df = pd.read_csv(fn)
+
+
+# hashing function
+# specifies to sha1
+def sha1_hash(dataframe, source_column, salt, new_column):
+    # salt ad id
+    salt = salt    # apply salt
+    dataframe[source_column] = dataframe[source_column].apply(lambda x: salt+x)    # set as column
+
+    # create hash column
+    dataframe[new_column] = [hashlib.sha1(str.encode(str(i))).hexdigest() for i in dataframe[source_column]]
+
+
+sha1_hash(
+    dataframe=ad_id,
+    source_column='',
+    salt='',
+    new_column='')
+
+
+ex_fn = ''
+df.to_csv(ex_fn, index=False)
